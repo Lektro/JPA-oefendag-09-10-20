98dbaa3806ad5b4e7703b4c1fc691d9a25eedbc3
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-10-16 12:42:13 +0200

    followed tutorial, still have a 404 error

--- src/main/java/de/ferienwohnungmosea/controller/IndexController.java
+++ src/main/java/de/ferienwohnungmosea/controller/IndexController.java
@@ -6,28 +6,19 @@ import de.ferienwohnungmosea.services.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
-@RequestMapping({ "/", "/index" })
 public class IndexController {
 
     @Autowired
     GuestService guestService;
 
-    @GetMapping("/")
-    public ModelAndView guest (Model model) {
+    @GetMapping("/addGuest")
+    public ModelAndView addGuest (Model model) {
         model.addAttribute("guest", new Guest());
-        return new ModelAndView("/");
-    }
-    @PostMapping("/")
-    public ModelAndView addGuest(@ModelAttribute("guest") Guest guest) {
-        guestService.addGuest(guest);
-        return new ModelAndView("redirect:/addGuest");
+        return new ModelAndView("addGuest");
     }
 }
