f31316e27f4012c17d8e17ca39fbff61e4a589df
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-10-16 13:28:42 +0200

    greeting works deleted the other classes

--- src/main/java/de/ferienwohnungmosea/SpringMvcApplication.java
+++ src/main/java/de/ferienwohnungmosea/SpringMvcApplication.java
@@ -1,4 +1,4 @@
-package de.ferienwohnungmosea.main;
+package de.ferienwohnungmosea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/de/ferienwohnungmosea/controller/IndexController.java
+++ src/main/java/de/ferienwohnungmosea/controller/IndexController.java
@@ -1,22 +1,15 @@
 package de.ferienwohnungmosea.controller;
 
 
-import de.ferienwohnungmosea.model.Guest;
-import de.ferienwohnungmosea.services.GuestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class IndexController {
 
-    @Autowired
-    GuestService guestService;
-
-    @GetMapping("/addGuest")
+    @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="NOOB")String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
