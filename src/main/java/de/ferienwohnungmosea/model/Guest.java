4e053ad83426ffeba0c2532fd247884fa1fe2945
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-10-16 15:05:38 +0200

    second page added after alot of adjustments, cant get it to work with java classes lol

--- src/main/java/de/ferienwohnungmosea/model/Guest.java
+++ src/main/java/de/ferienwohnungmosea/model/Guest.java
@@ -1,5 +1,50 @@
 package de.ferienwohnungmosea.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Guest {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+
+    public Guest(long id, String firstName) {
+        this.id = id;
+        this.firstName = firstName;
+    }
+    public Guest(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Guest{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/de/ferienwohnungmosea/repository/GuestRepository.java
@@ -0,0 +1,9 @@
+package de.ferienwohnungmosea.repository;
+
+import de.ferienwohnungmosea.model.Guest;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends CrudRepository<Guest, Long> {
+}
